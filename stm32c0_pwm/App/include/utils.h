a532a7dca1b812647ce2bde971e138b7750d5402
Wh31028 <wh31028@naver.com>
2025-08-04 17:53:29 +0900

    PWM

---
+++ stm32c0_pwm/App/include/utils.h
@@ -0,0 +1,18 @@
+/*
+ * utils.h
+ *
+ *  Created on: Aug 4, 2025
+ *      Author: wh31028
+ */
+
+#ifndef INCLUDE_UTILS_H_
+#define INCLUDE_UTILS_H_
+
+#include "def.h"
+
+void delay(uint32_t time_ms);
+uint32_t millis(void);
+
+
+
+#endif /* INCLUDE_UTILS_H_ */
