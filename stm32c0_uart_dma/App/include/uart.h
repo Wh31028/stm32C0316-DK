3a2e5f9f0818ed4c25304bc3f19351613cbbf9b0
wh31028 <wh31028@naver.com>
2025-08-14 20:54:14 +0900

    UART 수신 DMA

---
+++ stm32c0_uart_dma/App/include/uart.h
@@ -0,0 +1,24 @@
+/*
+ * uart.h
+ *
+ *  Created on: Aug 12, 2025
+ *      Author: wh310
+ */
+
+#ifndef INCLUDE_UART_H_
+#define INCLUDE_UART_H_
+
+#include "def.h"
+
+
+#define UART_MAX_CH         1
+
+
+
+bool     uartInit(void);
+uint32_t uartWrite(uint8_t ch,uint8_t* p_data,uint32_t length);
+uint32_t uartAvailable(uint8_t ch);
+uint8_t  uartRead(uint8_t ch);
+uint32_t uartPrintf(uint8_t ch,const char *fmt, ...);
+
+#endif /* INCLUDE_UART_H_ */
