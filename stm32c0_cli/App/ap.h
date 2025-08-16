2663c2a7c91ed6e0d73bdb502b1167d3813aed86
wh31028 <wh31028@naver.com>
2025-08-16 12:42:23 +0900

    CLI 기초

---
+++ stm32c0_cli/App/ap.h
@@ -0,0 +1,23 @@
+/*
+ * ap.h
+ *
+ *  Created on: Aug 4, 2025
+ *      Author: wh31028
+ */
+
+#ifndef AP_H_
+#define AP_H_
+
+
+#include "utils.h"
+#include "led.h"
+#include "pwm.h"
+#include "adc.h"
+#include "button.h"
+#include "uart.h"
+
+void apInit(void);
+void apMain(void);
+
+
+#endif /* AP_H_ */
